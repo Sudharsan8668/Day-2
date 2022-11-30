68af3049421c8f876a0b281c95d7e68d0ece42b2
Sudharsan8668 <119486589+Sudharsan8668@users.noreply.github.com>
2022-11-30 14:06:23 +0530

    Add files via upload

---
+++ Mycar.java
@@ -0,0 +1,24 @@
+package week1.day2;
+
+public class Mycar {
+       public void applyBreak() {
+    	   System.out.println("press break");
+       }
+       public void applyGear() {
+    	   System.out.println("press gear");
+       }
+       public void switchOnAc() {
+    	   System.out.println("on ac");
+       }
+       public void applyAcclerate() {
+    	   System.out.println("press acclerate");
+       }
+       public satic void main(String[] args) {
+    	   car car=new car();
+    	   car.applyBreak();
+    	   car.applyGear();
+    	   car.switchOnAc();
+    	   car.applyAcclerate();
+       }
+}
+
---
+++ mobile.java
@@ -0,0 +1,37 @@
+package seleniumproject;
+
+public class mobile {
+	   String mobileBrandName="Samsung";
+	   
+	   short noOfPouch=1;
+	   int noOfPiece=1;
+	   char firstLetter='S';
+	   double mobilePrice=2784.87;
+	   float mobileRange=23.3454F;
+	   long imeNumber=9087625251423L;
+	   boolean isDamaged=false;
+	   
+public static void 	main(String[] args) {
+	   Mobile mobile=new  Mobile();
+	   System.out.printIn(mobile.mobileBrandName);
+	   System.out.printIn(mobile.noOfpiece);
+	   System.out.printIn(mobile.noOfPouch);
+	   System.out.printIn(mobile.firstLetter);
+	   System.out.printIn(mobile.mobilePrice);
+	   System.out.printIn(mobile.mobileRange);
+	   System.out.printIn(mobile.imeNumber);
+	   System.out.printIn(mobile.isDamaged);
+	   
+}
+}
+	   
+	   
+	   
+	   
+	   
+	   
+	   
+	
+}
+
+}
